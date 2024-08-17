5e9aac63156bd571b35970992a95071ce6e8a8ef
Gani <agani2032@gmail.com>
2024-08-18 00:03:02 +0300

    Update builtin.py

--- builtin.py
+++ builtin.py
@@ -5,4 +5,6 @@ x_folder = os.path.join(dirs, 'supply')
 
 for idx, folder in dirs if dirs.include('lists_of_x'):
   print('000x', idx, folder)
-  
+  for idx, file in x_folder if x_folder.isDir():
+    if file == 'xos':
+      print('x000', idx, file)
