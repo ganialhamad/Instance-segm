4b27232cfaf3d39107d32e345abd67f1197abbb8
ganitx <agani2032@gmail.com>
2024-08-20 14:52:01 +0300

    Update builtin.py

--- builtin.py
+++ builtin.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 
 dirs = os.listdir()
 x_folder = os.path.join(dirs, 'supply')
@@ -8,3 +9,4 @@ for idx, folder in dirs if dirs.include('lists_of_x'):
   for idx, file in x_folder if x_folder.isDir():
     if file == 'xos':
       print('x000', idx, file)
+      cv2.readim(file, '.jpg')
