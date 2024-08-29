e0ec188739a3c8613c105fbff04ab6153dc3652c
Gani <agani2032@gmail.com>
2024-08-29 09:29:49 +0300

    Update train_net.py

--- train_net.py
+++ train_net.py
@@ -1,6 +1,8 @@
 const weeks = [1, 2, 3, 4, 5, 6, 7]
 const inCommingBookings = API_obj.bookings
 
-for (booing in inCommingBookings) {
-
+for (const idx, booking in inCommingBookings) {
+  if (booking.date == weeks[idx]) {
+    console.log("Logged: ", booking.date, booking.time);
+  }
 }
